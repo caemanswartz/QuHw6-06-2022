37dcbcf218911f47c36b35c3adc080ff9d81d236
caemanswartz <caemanswartz@gmail.com>
2022-06-04 10:08:22 -0700

    implemented GetAuthenticationStateAsync

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QuHWBlazorWasmJwtUse.Client;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider,AuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
